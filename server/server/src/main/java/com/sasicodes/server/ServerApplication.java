a32f682c5ca5ebe07f2ccd570737086a3581ae88
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2022-03-16 22:30:42 +0530

    controller and testing completed

--- server/server/src/main/java/com/sasicodes/server/ServerApplication.java
+++ server/server/src/main/java/com/sasicodes/server/ServerApplication.java
@@ -1,7 +1,13 @@
 package com.sasicodes.server;
 
+import com.sasicodes.server.model.Server;
+import com.sasicodes.server.repository.ServerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static com.sasicodes.server.enumeration.Status.SERVER_UP;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -10,4 +16,12 @@ public class ServerApplication {
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run(ServerRepository serverRepository)
+	{
+		return args -> {
+			serverRepository
+					.save(new Server(null, "192.168.1.160", "Linux", "16 GB", "PC", "http://localhost:8080/server/image/server1.png", SERVER_UP));
+		};
+	}
 }
--- server/server/src/main/java/com/sasicodes/server/controller/ServerController.java
+++ server/server/src/main/java/com/sasicodes/server/controller/ServerController.java
@@ -4,11 +4,17 @@ import com.sasicodes.server.model.Response;
 import com.sasicodes.server.model.Server;
 import com.sasicodes.server.service.implementation.ServerServiceImplementation;
 import lombok.AllArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.time.LocalDateTime;
 
 import static com.sasicodes.server.enumeration.Status.SERVER_UP;
@@ -23,13 +29,14 @@ import static org.springframework.http.HttpStatus.OK;
 public class ServerController {
 
     private final ServerServiceImplementation serverServiceImplementation;
+    private final ResourceLoader resourceLoader;
 
     @GetMapping("/list")
     public ResponseEntity<Response> getServers()
     {
         return ResponseEntity.ok(
                 Response.builder()
-                        .localDateTime(LocalDateTime.from(now()))
+                        .localDateTime(LocalDateTime.now())
                         .status(OK)
                         .statusCode(OK.value())
                         .message("servers retrieved")
@@ -43,7 +50,7 @@ public class ServerController {
         Server server = serverServiceImplementation.ping(ipAddress);
         return ResponseEntity.ok(
                 Response.builder()
-                        .localDateTime(LocalDateTime.from(now()))
+                        .localDateTime(LocalDateTime.now())
                         .status(OK)
                         .statusCode(OK.value())
                         .message(server.getStatus() ==SERVER_UP ? "Ping success" : "Ping Failure")
@@ -57,12 +64,48 @@ public class ServerController {
     public ResponseEntity<Response> getServers(@RequestBody @Valid  Server server) {
         return ResponseEntity.ok(
                 Response.builder()
-                        .localDateTime(LocalDateTime.from(now()))
+                        .localDateTime(LocalDateTime.now())
                         .status(CREATED)
                         .statusCode(CREATED.value())
                         .message("server created")
                         .data(of("server", serverServiceImplementation.createServer(server)))
                         .build()
         );
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Response> getServer(@PathVariable("id")Long id) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .localDateTime(LocalDateTime.now())
+                        .status(CREATED)
+                        .statusCode(CREATED.value())
+                        .message("server retrieved")
+                        .data(of("server", serverServiceImplementation.get(id)))
+                        .build()
+        );
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Response> deleteServer(@PathVariable("id")Long id) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .localDateTime(LocalDateTime.now())
+                        .status(CREATED)
+                        .statusCode(CREATED.value())
+                        .message("server deleted")
+                        .data(of("deleted", serverServiceImplementation.delete(id)))
+                        .build()
+        );
+    }
+
+    @GetMapping(path = "/image/{fileName}", produces = {MediaType.IMAGE_PNG_VALUE})
+    public byte[] getServerImage(@PathVariable("fileName")String  fileName) throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:"+fileName);
+        byte[] bdata;
+        try (InputStream inputStream = resource.getInputStream()) {
+            bdata = FileCopyUtils.copyToByteArray(inputStream);
+        }
+        return bdata;
     }
 }
